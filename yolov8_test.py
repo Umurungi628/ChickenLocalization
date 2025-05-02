190124f40a3e7908999d83816f5356dcc4175a1d
Umurungi628 <sandumuru@gmail.com>
2025-05-02 13:15:58 +0200

    Delete yolov8_test.py

--- yolov8_test.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a YOLOv8 model (yolov8n.pt is a small model, consider using yolov8m.pt or yolov8l.pt for larger models)
-model = YOLO('yolov8m.pt')
-
-# Train the model
-model.train(data='/Users/User/PycharmProjects/ChickenLocalization/ChickenLocalization-main/data.yaml', epochs=100, imgsz=640, batch=16)
